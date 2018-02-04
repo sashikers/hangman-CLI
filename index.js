d90849447a523596ea6ad83b5c08ee52db263e57
Sasha <sashikers@gmail.com>
2018-02-03 18:57:18 -0800

    final

--- index.js
+++ index.js
@@ -1,74 +1,39 @@
 var Word = require("./word.js");
 var inquirer = require("inquirer");
 
-var guestCount = 0;
-
-// var words = ["ANCHOR", "ALMANAC", "BAREBOTTLE", "CELLARMAKER", "HARMONIC", "MAGNOLIA", "THIRSTYBEAR", "FORTPOINT","PIZZAPORT","SIERRANEVADA","ROLLINGROCK"];
-
-// var randomIndex = (Math.floor(Math.random() * words.length)) + 1;
-// var guessString = words[randomIndex];
-// var wordToGuess = new Word(guessString);
-// console.log("wordToGuess",wordToGuess);
-
 var wordToGuess = "";
 
-function startNewGame() {
+function generateNewWord() {
+	console.log("==============================================================");
+	console.log("==============================================================");
+	console.log("==========================New word!===========================");
+	console.log("==============================================================");
+	console.log("==============================================================");
 
-	var startNewGame = this;
-	// console.log("startNewGame", startNewGame);
-
-	var guesses = 0;
-	var guessedLetters = "";
-	var guessedCorrectly = 0;
-	console.log("guessedCorrectly", guessedCorrectly);
+	var generateNewWord = this;
 
 	var words = ["ANCHOR", "ALMANAC", "BAREBOTTLE", "CELLARMAKER", "HARMONIC", "MAGNOLIA", "THIRSTYBEAR", "FORTPOINT","PIZZAPORT","SIERRANEVADA","ROLLINGROCK"];
 	var randomIndex = (Math.floor(Math.random() * words.length)) + 1;
 	var guessString = words[randomIndex];
-	wordToGuess = new Word(guessString);
-	// console.log("wordToGuess",wordToGuess);
+	var wordToGuess = new Word(guessString);
+	var guesses = 0;
+	var allGuessedLetters = [];
+	// console.log("allGuessedLetters",allGuessedLetters);
 
-	console.log("==============================================================================================================================================================================================");
+	userGuess(wordToGuess, guesses);
 
-	if (guesses < 3) {
-		inquirer.prompt([
-			{
-				name: "guess",
-				message: "Guess a letter!"
-			}
-		])
-		.then(function(answer) {
-			console.log("answer", answer);
-			wordToGuess.checkCharacter(answer.guess.toUpperCase());
-			guessedLetters += answer.guess;
-			guessedLetters += " ";
-			console.log("guessedLetters", guessedLetters.toUpperCase());
-			guesses++;
-
-			console.log("guessedCorrectly", this.guessedCorrectly);
-
-			// recursion 
-			userGuess();
-		});
-	} 
-	else {
-		console.log("You failed! Try a new game.");
-		guesses = 0;
-		console.log("guesses", guesses);
-
-	}	
 }
+function userGuess(guessableWord, guesses) {
 
-
-function userGuess() {
-	var thisUserGuess = this;
-	// console.log("==============================================================================================================================================================================================", thisUserGuess);
 	
-	wordToGuess.showWord();
+	console.log("==============================================================");
+	console.log("==============================================================");
+	console.log("Guesses left: ", 10 - guesses);
 
-	console.log("startNewGame.guesses", startNewGame.guesses);
+	var guessedLetters = "";
+	guessableWord.showWord();
 
-	if (startNewGame.guesses < 3) {
+	if (guesses < 10) {
 		
 		inquirer.prompt([
 			{
@@ -78,25 +43,26 @@ function userGuess() {
 		])
 		.then(function(answer) {
 			console.log("answer", answer);
-			wordToGuess.checkCharacter(answer.guess.toUpperCase());
+			guessableWord.checkCharacter(answer.guess.toUpperCase());
 			guessedLetters += answer.guess;
 			guessedLetters += " ";
 			console.log("guessedLetters", guessedLetters.toUpperCase());
+
 			guesses++;
 
-			console.log("guessedCorrectly", this.guessedCorrectly);
-
 			// recursion 
-			userGuess();
+			userGuess(guessableWord, guesses);
+
+			if(guessableWord.guessedWord === true) {
+				console.log("guessableWord.guessedWord true");
+				generateNewWord();
+			} 
 		});
 	} 
 	else {
 		console.log("You failed! Try a new game.");
-		guesses = 0;
-		console.log("guesses", guesses);
-
+		generateNewWord();
 	}
 }
 
-startNewGame();
-// userGuess();
+generateNewWord();
